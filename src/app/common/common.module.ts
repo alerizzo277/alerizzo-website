e897c86614697ad2fda17bef5cc64fd4007dbd0a
alerizzo277 <you@example.com>
2024-11-16 11:42:58 +0100

    set up home page and style.css

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HomePageComponent
   ],
   imports: [
     IonicModule,
     TranslateModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    HomePageComponent
   ]
 })
 export class CommonModule { }
